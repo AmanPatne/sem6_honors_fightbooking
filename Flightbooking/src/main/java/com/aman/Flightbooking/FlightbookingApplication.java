826b50d294044befa9b4bd847d94ea7bfa1dc269
AmanPatne <apatne78@gmail.com>
2025-05-04 01:26:12 +0530

    Add new controllers, models, services, and updated main application file

--- Flightbooking/src/main/java/com/aman/Flightbooking/FlightbookingApplication.java
+++ Flightbooking/src/main/java/com/aman/Flightbooking/FlightbookingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightBookingApplication {
+public class FlightbookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightBookingApplication.class, args);
+		SpringApplication.run(FlightbookingApplication.class, args);
 	}
 
 }
